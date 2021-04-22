98c7c65d7efee820b29e763f125e4108b71de428
Joshua <77082071+avav1818@users.noreply.github.com>
2021-04-22 17:42:27 +0100

    Update scrapper001.py

--- web_scraping/scrapper001.py
+++ web_scraping/scrapper001.py
@@ -17,14 +17,14 @@ def scrape_price():
     cookie_button = "/html/body/div/div/div/div/form/div[2]/div[2]/button" # Accepting cookies
     cookies = driver.find_element_by_xpath(cookie_button).click()
     time.sleep(2) # Explicit wait for webpage to wait 
-    
-    price_elem = driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')
-    price_val = price_elem.text
 
     # Appending price information to a .txt file every 5 minutes and refreshing the driver 
     while True:
         time.sleep(300 - time.time() % 300)
         
+        price_elem = driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')
+        price_val = price_elem.text
+
         with open("gjPriceData.txt", mode="a", encoding="utf-8") as f:
             time_format = time.strftime("%d:%m:%Y - %H:%M:%S")
 
