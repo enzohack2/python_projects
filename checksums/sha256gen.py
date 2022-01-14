8642f773ae33549e8b352662ffca9b647fd25905
ZimCanIT <77082071+ZimCanIT@users.noreply.github.com>
2022-01-14 20:17:35 +0000

    Add files via upload

--- checksums/sha256gen.py
+++ checksums/sha256gen.py
@@ -3,11 +3,12 @@
 import hashlib
 import sys
 
-source_checksum = input("[#] Enter download source checksum: ")
-fpath = input("[#] Enter ABSOLUTE filepath for file to generate checksum: ")
+source_checksum = input("[#] Enter source checksum: ")
+fpath = input("[#] ABSOLUTE filepath to file from which a checksum will be generated: ")
 
 def checksum_verifier(checksum, filepath):
-    """Generates a SHA-256 checksum from file in @filepath and compares it to input @checksum and informs the user if checksums match or otherwise."""
+    """Generates a SHA-256 checksum from file in @filepath and copmares
+    it to input @checksum. Informs user if checksum match or otherwise."""
 
     print("[#] Generating a checksum...")
 
@@ -18,20 +19,23 @@ def checksum_verifier(checksum, filepath):
         hash_obj.update(data)
         generated_checksum = hash_obj.hexdigest()
 
-        print(f"[#] Generated checksum: {generated_checksum}")
-
+        print(f"[#] Generated chechsum: {generated_checksum}")
+    
+    # Exception handling
     try:
         assert generated_checksum == checksum
-        print("[#] Yay! Generated checksum matches download source SHA-256 checksum. Good day :)")
+        print("[#] Congratulations, the source checksum and application generated checksum match")
         sys.exit(1)
+
     except AssertionError:
-        print("[#] Generated SHA-256 checkusm doesn't match with source checksum. Potential issues listed below...")
-        print()
-        print("[#] Check your inputs are correct, both the download source SHA-256 checksum and the correct filepath")
-        print("[#] The file is possibly corrupt, did you download it from it's original source or an unregistered download mirror?")
-        print("[#] Redownload your file and try the program again")
-        print("[#] Good day :)")
+        print("[#] Generated SHA-256 checksum doesn't match with source checksum. Potential issues listed below:")
+        print() # spacing
+        print("[#] Ceck both inputs are correct: source checksum and filepath leading to executable on your device.")
+        print("[#] Redownload your file and try the program again. Good day :)")
         sys.exit(1)
 
 if __name__ == "__main__":
     checksum_verifier(source_checksum, fpath)
+
+
+# Script works. Thanks for watching :D
